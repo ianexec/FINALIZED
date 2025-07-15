3b6b66aa6b37479721f71ffa6153b3907fb15734
ianexec <newvpnlunatix293@gmail.com>
2025-07-15 08:15:25 +0700

    Create setup.sh

---
+++ LTbotVPN/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+   
+   wget https://raw.githubusercontent.com/ianexec/FINALIZED/main/LTbotVPN/SHELLBOT
+    unzip SHELLBOT
+    chmod +x LTBOTVPN/*
+    mv LTBOTVPN /usr/bin
+
+    # HAPUS EXTRAK
+    rm -rf SHELLBOT
+      
